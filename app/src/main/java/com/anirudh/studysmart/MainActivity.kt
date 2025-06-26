316141439d1ae976ac5b075564b2268534b51354
Anirudh <anirudhlohiya999@gmail.com>
2025-06-26 14:56:12 +0530

    Creating the subject screen where user can see details related to a particular subject.

--- app/src/main/java/com/anirudh/studysmart/MainActivity.kt
+++ app/src/main/java/com/anirudh/studysmart/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.anirudh.studysmart.domain.model.Session
+import com.anirudh.studysmart.domain.model.Subject
+import com.anirudh.studysmart.domain.model.Task
+import com.anirudh.studysmart.presentation.Subject.SubjectScreen
 import com.anirudh.studysmart.presentation.dashboard.DashboardScreen
 import com.anirudh.studysmart.presentation.theme.StudySmartTheme
 
@@ -13,8 +17,101 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StudySmartTheme {
-                DashboardScreen()
+                SubjectScreen()
             }
         }
     }
 }
+
+
+val subjects = listOf(
+    Subject("Math", 10f, Subject.subjectCardColors[0],0),
+    Subject("Science", 10f, Subject.subjectCardColors[1],0),
+    Subject("English", 10f, Subject.subjectCardColors[2],0),
+    Subject("Computer", 10f, Subject.subjectCardColors[3],0),
+    Subject("History", 10f, Subject.subjectCardColors[4],0),
+)
+
+val tasks = listOf(
+    Task(
+        title = "Complete Math Assignment",
+        description = "Solve all the problems in chapter 5",
+        dueDate = 0L,
+        priority = 2,
+        relatedToSubject = "",
+        isComplete = false,
+        taskSubjectId = 0,
+        taskId = 0
+    ),
+    Task(
+        title = "Do HomeWork",
+        description = "Solve all the problems in chapter 5",
+        dueDate = 0L,
+        priority = 1,
+        relatedToSubject = "",
+        isComplete = true,
+        taskSubjectId = 0,
+        taskId = 0
+    ),
+    Task(
+        title = "Make Project",
+        description = "Solve all the problems in chapter 5",
+        dueDate = 0L,
+        priority = 0,
+        relatedToSubject = "",
+        isComplete = false,
+        taskSubjectId = 0,
+        taskId = 0
+    ),
+    Task(
+        title = "Buy Groceries",
+        description = "Solve all the problems in chapter 5",
+        dueDate = 0L,
+        priority = 1,
+        relatedToSubject = "",
+        isComplete = false,
+        taskSubjectId = 0,
+        taskId = 0
+    ),
+    Task(
+        title = "Watch Movie",
+        description = "Solve all the problems in chapter 5",
+        dueDate = 0L,
+        priority = 2,
+        relatedToSubject = "",
+        isComplete = false,
+        taskSubjectId = 0,
+        taskId = 0
+    )
+)
+
+val sessions = listOf(
+    Session(
+        relationToSubject = "English",
+        date = 0L,
+        duration = 2,
+        sessionSubjectId = 0,
+        sessionId = 0,
+    ),
+    Session(
+        relationToSubject = "Math",
+        date = 0L,
+        duration = 1,
+        sessionSubjectId = 0,
+        sessionId = 0,
+    ),
+    Session(
+        relationToSubject = "Science",
+        date = 0L,
+        duration = 3,
+        sessionSubjectId = 0,
+        sessionId = 0,
+    ),
+    Session(
+        relationToSubject = "Computer",
+        date = 0L,
+        duration = 2,
+        sessionSubjectId = 0,
+        sessionId = 0,
+    ),
+)
---
+++ app/src/main/java/com/anirudh/studysmart/presentation/Subject/SubjectScreen.kt
@@ -0,0 +1,266 @@
+package com.anirudh.studysmart.presentation.Subject
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.anirudh.studysmart.domain.model.Subject
+import com.anirudh.studysmart.presentation.components.AddSubjectDialog
+import com.anirudh.studysmart.presentation.components.CountCard
+import com.anirudh.studysmart.presentation.components.DeleteDialog
+import com.anirudh.studysmart.presentation.components.studySessionsList
+import com.anirudh.studysmart.presentation.components.tasksList
+import com.anirudh.studysmart.sessions
+import com.anirudh.studysmart.tasks
+
+@Preview
+@Composable
+private fun SubjectScreenPreview(){
+    SubjectScreen()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SubjectScreen(){
+
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val listState = rememberLazyListState()
+    val isFABExtended by remember { derivedStateOf { listState.firstVisibleItemIndex == 0 } }
+
+    var isEditSubjectDialogOpen by rememberSaveable { mutableStateOf(false) }
+    var isDeleteSessionDialogOpen by rememberSaveable { mutableStateOf(false) }
+    var isDeleteSubjectDialogOpen by rememberSaveable { mutableStateOf(false) }
+
+    var subjectName by remember { mutableStateOf("") }
+    var goalHours by remember { mutableStateOf("") }
+    var selectedColor by remember { mutableStateOf(Subject.subjectCardColors.random()) }
+
+    AddSubjectDialog(
+        isOpen = isEditSubjectDialogOpen,
+        selectedColors = selectedColor,
+        subjectName = subjectName,
+        goalHours = goalHours,
+        onColorChange = {selectedColor = it },
+        onSubjectNameChange = {subjectName = it},
+        onGoalHoursChange = {goalHours = it},
+        onDismissRequest = { isEditSubjectDialogOpen = false },
+        onConformButtonClick = { isEditSubjectDialogOpen = false }
+    )
+
+    DeleteDialog(
+        isOpen = isDeleteSubjectDialogOpen,
+        title = "Delete Subject?",
+        onDismissRequest = {isDeleteSubjectDialogOpen = false},
+        bodyText = "Are you sure you want to delete this subject? All related tasks and study " +
+                "sessions will be deleted. This action cannot be undone",
+        onConformButtonClick = {isDeleteSubjectDialogOpen = false}
+    )
+
+    DeleteDialog(
+        isOpen = isDeleteSessionDialogOpen,
+        title = "Delete Session?",
+        onDismissRequest = {isDeleteSessionDialogOpen = false},
+        bodyText = "Are you sure you want to delete this subject? This will reduce your study " +
+                "hours. This action cannot be undone",
+        onConformButtonClick = {isDeleteSessionDialogOpen = false}
+    )
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SubjectScreenTopBar(
+                title = "Mathematics",
+                onBackButtonClick = { /* Handle back button click */ },
+                onDeleteButtonClick = { isDeleteSubjectDialogOpen = true },
+                onEditButtonClick = { isEditSubjectDialogOpen = true },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {},
+                icon = {Icon(imageVector = Icons.Default.Add, contentDescription = "Add Task")},
+                text = { Text(text = "Add Task") },
+                expanded = isFABExtended
+            )
+        }
+    ) { paddingValue ->
+        LazyColumn(
+            state = listState,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValue)
+        ) {
+            item {
+                SubjectOverviewSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    studiedHours = "20",
+                    goalHours = "50",
+                    progress = 0.4f
+                )
+            }
+            tasksList(
+                sectionTitle = "Upcoming Tasks",
+                emptyListText = "You don't have any upcoming tasks.\n " +
+                        "Click the + button to add the new task.",
+                tasks = tasks,
+                onCheckBoxClick = {},
+                onTaskCardClick = {}
+            )
+            item{
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            tasksList(
+                sectionTitle = "Competed Tasks",
+                emptyListText = "You don't have any completed tasks.\n " +
+                        "Click the check box to complete a task.",
+                tasks = tasks,
+                onCheckBoxClick = {},
+                onTaskCardClick = {}
+            )
+            item{
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            studySessionsList(
+                sectionTitle = "Recent Study Sessions",
+                emptyListText = "You don't have any recent study sessions.\n " +
+                        "Start a study session to begin recording your progress.",
+                sessions = sessions,
+                onDeleteIconClick = {isDeleteSessionDialogOpen = true }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SubjectScreenTopBar(
+    title: String,
+    onBackButtonClick: () -> Unit,
+    onDeleteButtonClick: () -> Unit,
+    onEditButtonClick: () -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior
+){
+    LargeTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.headlineSmall
+            ) },
+        navigationIcon = {
+            IconButton(onClick = onBackButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back Button"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onDeleteButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Back Button"
+                )
+            }
+            IconButton(onClick = onEditButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Back Button"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+private fun SubjectOverviewSection(
+    modifier: Modifier,
+    studiedHours: String,
+    goalHours: String,
+    progress: Float
+){
+    val percentageProgress = remember(progress) {
+        (progress * 100).toInt().coerceIn(0,100)
+    }
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Study Hours",
+            count = goalHours
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Study Hours",
+            count = studiedHours
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Box(
+            modifier = Modifier.size(75.dp),
+            contentAlignment = Alignment.Center
+        ){
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = progress,
+                strokeWidth = 4.dp,
+                strokeCap = StrokeCap.Round,
+                color = MaterialTheme.colorScheme.surfaceVariant
+            )
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = progress,
+                strokeWidth = 4.dp,
+                strokeCap = StrokeCap.Round
+            )
+            Text(text = "$percentageProgress%")
+        }
+
+    }
+}
