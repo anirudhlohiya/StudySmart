48bd85dd8c29980f87f572c2342d3c9b4d066be8
Anirudh <anirudhlohiya999@gmail.com>
2025-07-06 00:10:16 +0530

    Setting up the room database

---
+++ app/src/main/java/com/anirudh/studysmart/data/local/TaskDao.kt
@@ -0,0 +1,28 @@
+package com.anirudh.studysmart.data.local
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.anirudh.studysmart.domain.model.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Upsert
+    suspend fun upsertTask(task: Task)
+
+    @Query("DELETE FROM Task WHERE taskId = :taskId")
+    suspend fun deleteTask(taskId: Int)
+
+    @Query("DELETE FROM Task WHERE taskSubjectId = :subjectId")
+    suspend fun deleteTasksBySubjectId(subjectId: Int)
+
+    @Query("SELECT * FROM Task WHERE taskId = :taskID")
+    suspend fun getTaskById(taskID: Int): Task?
+
+    @Query("SELECT * FROM Task WHERE taskSubjectId = :subjectId")
+    suspend fun getTasksForSubjectId(subjectId: Int): Flow<List<Task>>
+
+    @Query("SELECT * FROM Task")
+    fun getAllTasks(): Flow<List<Task>>
+}
